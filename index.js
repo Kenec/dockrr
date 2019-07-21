9e33d7c5004e25d67926b5618f91d542822b80ae
Kenec <nnamani.kenechukwu@gmail.com>
2019-07-21 21:13:21 +0100

    chore: setup project linting setup dockerfile constants project structure setup

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Main function
+ * @param {object} config
+ * @return {null}
+ */
+function main(config) {
+  // TODO: use the config to generate a Dockerfile
+  console.log("Index File:");
+}
